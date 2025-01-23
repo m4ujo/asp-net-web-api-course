0bf9d01aee18702f40181402967ea1b36693d86c
m4ujo <mauriciocast11@outlook.com>
2025-01-23 15:29:40 -0500

    feat: add SQL Server database support

---
+++ Data/VideoGameDbContext.cs
@@ -0,0 +1,42 @@
+﻿using asp_net_web_api_course.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace asp_net_web_api_course.Data
+{
+  public class VideoGameDbContext(DbContextOptions<VideoGameDbContext> options) : DbContext(options)
+  {
+    public DbSet<VideoGame> VideoGames => Set<VideoGame>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+
+      modelBuilder.Entity<VideoGame>().HasData(
+        new VideoGame
+        {
+          Id = 1,
+          Title = "The Legend of Zelda: Breath of the Wild",
+          Platform = "Nintendo Switch",
+          Developer = "Nintendo EPD",
+          Publisher = "Nintendo"
+        },
+        new VideoGame
+        {
+          Id = 2,
+          Title = "Super Mario Odyssey",
+          Platform = "Nintendo Switch",
+          Developer = "Nintendo EPD",
+          Publisher = "Nintendo"
+        },
+        new VideoGame
+        {
+          Id = 3,
+          Title = "The Witcher 3: Wild Hunt",
+          Platform = "PlayStation 4",
+          Developer = "CD Projekt Red",
+          Publisher = "CD Projekt"
+        }
+      );
+    }
+  }
+}
---
+++ Migrations/20250123202706_Seeding.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace asp_net_web_api_course.Migrations
+{
+    /// <inheritdoc />
+    public partial class Seeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "VideoGames",
+                columns: new[] { "Id", "Developer", "Platform", "Publisher", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Nintendo EPD", "Nintendo Switch", "Nintendo", "The Legend of Zelda: Breath of the Wild" },
+                    { 2, "Nintendo EPD", "Nintendo Switch", "Nintendo", "Super Mario Odyssey" },
+                    { 3, "CD Projekt Red", "PlayStation 4", "CD Projekt", "The Witcher 3: Wild Hunt" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "VideoGames",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "VideoGames",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "VideoGames",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
